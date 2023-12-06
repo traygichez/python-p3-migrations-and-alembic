4710f54b362ea24fc48df1e54fd4f1804d9c2316
tray <onesimus.githua@student.moringaschool.com>
2023-12-06 12:59:47 +0300

    completed assignment

---
+++ app/migrations/versions/43bc56ce47a5_empty_init.py
@@ -0,0 +1,24 @@
+"""Empty Init
+
+Revision ID: 43bc56ce47a5
+Revises: 
+Create Date: 2023-12-06 12:50:47.995351
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '43bc56ce47a5'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade() -> None:
+    pass
+
+
+def downgrade() -> None:
+    pass
